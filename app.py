2d581ede87405a731cbb5c5e90b02ac9560278cb
Andrew Zaiko <andrewzaiko@gmail.com>
2022-09-11 19:07:22 +0300

    Добавил namespace Movies

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 # app.py
 
-from flask import Flask, request
+from flask import Flask
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
@@ -8,6 +9,7 @@ from marshmallow import Schema, fields
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
 db = SQLAlchemy(app)
 
 
@@ -24,6 +26,22 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Str()
+    director_id = fields.Int()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +53,26 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+api = Api(app)
+movies_namespace = api.namespace("movies")
+
+
+@movies_namespace.route("/")
+class MoviesView(Resource):
+    def get(self):
+        movies = db.session.query(Movie).all()
+        return movies_schema.dump(movies), 200
+
+
+@movies_namespace.route("/<int:id>")
+class MovieView(Resource):
+    def get(self, id):
+        movie = db.session.query(Movie).get(id)
+        if movie:
+            return movie_schema.dump(movie), 200
+        else:
+            return "Такого ID не существует", 404
 
 
 if __name__ == '__main__':
